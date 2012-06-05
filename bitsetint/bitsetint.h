c15c1df767aa119a0ca9d181674487ed4e242759
Lee Elenbaas <lee.elenbaas@gmail.com>
2012-06-05 23:50:50 +0300

    transform the += implementation to template meta progrmming and make it constexpr

--- bitsetint/bitsetint.h
+++ bitsetint/bitsetint.h
@@ -27,6 +27,14 @@ namespace math {
 namespace bitsetint {
 
 template<size_t w>
+class bitsetint;
+
+template <size_t w, size_t i>
+constexpr bool add(bitsetint<w>&, const bitsetint<w>&);
+template <size_t w, size_t i>
+constexpr bool sub(bitsetint<w>&, const bitsetint<w>&);
+
+template<size_t w>
 class bitsetint
 {
 public:
@@ -34,61 +42,54 @@ public:
   constexpr bitsetint(unsigned long v) : _val(v) { }
   //constexpr bitsetint(std::string v) : _val(v) { } // need to convert base10 to binary, or use conversions
   
-  constexpr bitsetint(const bitsetint<w>& o) : _val(o._val) { }
   constexpr bitsetint(bitsetint<w>&& o) : _val(std::move(o._val)) { }
 
   template<size_t ow>
-  constexpr bitsetint(const bitsetint<ow>& o) : _val(o._val.to_string()) { static_assert(ow <= w, "Can only implicitly convert bitsetint to longer width"); }
+  constexpr bitsetint(const bitsetint<ow>& o) : _val(o._val) { static_assert(ow <= w, "Can only implicitly convert bitsetint to longer width"); }
   
   template<size_t ow>
   explicit operator bitsetint<ow>() { return bitsetint<ow>(_val.to_string().substring(ow>=w?0:w-ow)); }
   
-  bitsetint<w>& operator += (const bitsetint<w>& o) {
-    std::bitset<3> adder;
-    
-    for(int i=0;i<w; ++i){
-      adder[0] = _val[i];
-      adder[1] = o._val[i];
-      
-      _val[i] = (adder.count() != 2); // bit result
-      adder[2] = (adder.count() >= 2); // carry
-    }
-    
-    _val[w] = adder[2];
-    
-    return *this;
+  constexpr bitsetint<w>& operator+= (const bitsetint<w>& o) {
+    static_assert(w>=1, "bitsetint of size 0 or less does not support arithmetic operations(+)");
+    return (add<w,w>(*this, o))?*this:*this;
   }
+  template <size_t i>
+  friend bool add(bitsetint<w>&, const bitsetint<w>&);
   
-  bitsetint<w>& operator -= (const bitsetint<w>& o) {
-    std::bitset<3> subber;
-    
-    for(int i=0;i<w; ++i){
-      subber[0] = _val[i];
-      subber[1] = o._val[i];
-      
-      _val[i] = (subber.count() != 2); // bit result
-      subber[2] = ((subber[0] && subber.all()) || (!subber[0] && subber.any())); // carry
-    }
-    
-    _val[w] = subber[2];
-    
-    return *this;
+  constexpr bitsetint<w>& operator-= (const bitsetint<w>& o) {
+    static_assert(w>=1, "bitsetint of size 0 or less does not support arithmetic operations(-)");
+    return (sub<w,w>(*this, o))?*this:*this;
   }
+  template <size_t i>
+  friend bool sub(bitsetint<w>&, const bitsetint<w>&);
 private:
-  std::bitset<w+1> _val; // bitset is set to w+1 for carry placeholder
+  typedef std::bitset<w+1> bitfield;
+  typedef typename bitfield::reference bitreference;
+  
+  bitfield _val; // bitset is set to w+1 for carry placeholder
 };
 
 template<size_t w>
-bitsetint<w> operator+(const bitsetint<w>& lhs, const bitsetint<w>& rhs) {
-  auto res = lhs;
-  return res += rhs;
-}
+constexpr bitsetint<w> operator+(const bitsetint<w>& lhs, const bitsetint<w>& rhs) { return bitsetint<w>(lhs) += rhs; }
 
 template<size_t w>
-bitsetint<w> operator-(const bitsetint<w>& lhs, const bitsetint<w>& rhs) {
-  auto res = lhs;
-  return res -= rhs;
-}
+constexpr bitsetint<w> operator-(const bitsetint<w>& lhs, const bitsetint<w>& rhs) { return bitsetint<w>(lhs) -= rhs; } 
+
+template<size_t w>
+constexpr bool adder(typename bitsetint<w>::bitreference b1, bool b2, bool b3) { return std::pair<bool,bool>( (b1 && b2) || (b1 && b3) || (b2 && b3), b1 = (b1 != b2 != b3)).first; }
+template<size_t w>
+constexpr bool suber(typename bitsetint<w>::bitreference b1, bool b2, bool b3) { return std::pair<bool,bool>( (!b1 && b2) || (!b1 && b3) || (b2 && b3), b1 = (1==(((b1?1:0)+(b2?1:0)+(b3?1:0))%2))).first; }
+
+template <size_t w, size_t i>
+constexpr bool add(bitsetint<w>& s, const bitsetint<w>& o) { return adder(s._val[i], o._val[i], add<w, i-1>(s, o)); }
+template <size_t w>
+constexpr bool add<w, 0>(bitsetint<w>& s, const bitsetint<w>& o) { return adder(s._val[0], o._val[0], false); }
+
+template <size_t w, size_t i>
+constexpr bool sub(bitsetint<w>& s, const bitsetint<w>& o) { return suber(s._val[i], o._val[i], sub<w, i-1>(o)); }
+template <size_t w>
+constexpr bool sub<w, 0>(bitsetint<w>& s, const bitsetint<w>& o) { return suber(s._val[0], o._val[0], false); }
 
 } /* namespace bitsetint */
 } /* namespace math */
