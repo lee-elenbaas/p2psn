1ff1e327e1902ed52682bbfb8c63c4574142cd64
Lee Elenbaas (HT-mc) <l.e@ht-mc.com>
2012-06-24 16:55:17 +0300

    Place project and copywrite information info in source files

--- polynomial/full_polynomial.cpp
+++ polynomial/full_polynomial.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2012  <copyright holder> <email>
+    Peer 2 Peer Social Network - Infrastracture for big brother free social network.
+    Copyright (C) 2012  Lee Elenbaas - lee [dot] elenbaas [at] gmail [dot] com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
