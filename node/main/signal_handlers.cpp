00a433fa8671af5c9a3b8a02f55774425315e0d4
Lee Elenbaas <lee.elenbaas@gmail.com>
2012-07-04 00:50:44 +0300

    continue signal implementation

--- node/main/signal_handlers.cpp
+++ node/main/signal_handlers.cpp
@@ -1,4 +1,13 @@
 
 #include "signal_handlers.h"
 
+// TODO: need to handle win32 case using #ifdef
+#include <signal.h>
+#include <errono.h>
+
+signal_handler::signal_handler(sppcms::service& srv, int sig) throw (signal_setup_exception)
+	: _srv(srv), _signal(sig)
+{
+	signal(sig, signal_handler::handler)
+}
 
--- node/main/signal_handlers.h
+++ node/main/signal_handlers.h
@@ -2,10 +2,26 @@
 #ifndef __SIGNAL_HANDLERS_H
 #define __SIGNAL_HANDLERS_H
 
+#include <stdexcept>
+
+class signal_setup_exception : public std::runtime_error {
+public:
+	signal_setup_exception(const std::string& message) : std::runtime_error(message) {}
+};
+
 class signal_handler {
 public:
+	signal_handler(sppcms::service& srv, int signal) throw (signal_setup_exception);
+	~signal_handler();
+
+	bool signal_recieved() const { return _signal_recieved; }
+	void reset_signal() { _signal_recieved = false; }
 private:
-	cppcms::service
-}
+	cppcms::service& _srv;
+	int _signal;
+	bool _signal_recieved = false;
+
+	void handler();
+};
 
 #endif // __SIGNAL_HANDLERS_H
