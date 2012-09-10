5ffc8e72f416347340cc73cfba718c8c4738dec0
Lee Elenbaas <lee.elenbaas@gmail.com>
2012-09-11 00:11:20 +0300

    hashing gives string - just not the expected string

--- node/utils/crypto.cpp
+++ node/utils/crypto.cpp
@@ -1,9 +1,10 @@
 #include "crypto.hpp"
 
-#include "base64.hpp"
+#include <sstream>
+#include <iomanip>
 
 using namespace p2psn::utils;
-using std::string;
+using namespace std;
 using cppcms::crypto::message_digest;
 
 string signature::md5(const string& message) {
@@ -15,15 +16,24 @@ string signature::sha1(const string& message) {
 }
 
 string signature::generate(std::auto_ptr<cppcms::crypto::message_digest> generator, const std::string& message) {
-	unsigned hash_size = 2+generator->digest_size()/sizeof(char);
+	unsigned hash_size = generator->digest_size();
+	unsigned array_size = hash_size/sizeof(unsigned char);
 
-	char hash[hash_size];
-	hash[hash_size-1]=hash[hash_size-2]=0;
+	if ((hash_size%sizeof(unsigned char))>0)
+		++array_size;
+
+	unsigned char hash[array_size];
 
 	generator->append(message.c_str(), message.size()*sizeof(char));
 	generator->readout(hash);
 
-// TODO: remove the base64 and simply use itoa, or ostringstream to export the ints into the string
-	return base64_encode(hash, hash_size);
+	ostringstream os;
+
+	os << "hash \""<< message<< "\" hash_size="<<hash_size<<" array_size="<<array_size<<" hash:";
+
+	for (unsigned i=0; i<hash_size;++i)
+		os << setw(2) << setfill('0') << hex << static_cast<unsigned>(hash[i]);
+
+	return os.str();
 }
 
