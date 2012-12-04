6be44d5d0fa28b94c540ef88115ad7e68d5b1bf3
Lee Elenbaas <lee@elenbaas.org.il>
2012-12-04 15:36:05 +0200

    Palce filtering logic inside the runtime hirarchy base and not inheritance base

--- node/src/node/admin_menu/admin_menu_app.cpp
+++ node/src/node/admin_menu/admin_menu_app.cpp
@@ -8,6 +8,20 @@ using namespace p2psn::node_admin;
 using cppcms::json::value;
 using std::string;
 
+void admin_menu_app::main(std::string request_url)
+{
+    // TODO: add ip filtering here
+    DEBUG("Admin from ip: "+request().remote_addr());
+    
+    if (session().is_set("user")) {
+        base_app::main(request_url);
+    }
+    else {
+        session().set("url_after_login", request().path_info());
+        response_redirect(url("/node/login"));
+    }
+}
+
 admin_menu_app::admin_menu_app(cppcms::service &srv) 
     : admin_base_app(srv) 
 {
