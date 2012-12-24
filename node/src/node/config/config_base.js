153ac7dc40173e3ab1fd52dc6a0873a93de3094f
Lee Elenbaas <lee@elenbaas.org.il>
2012-12-24 23:59:58 +0200

    added favicon.ico file - does not work properly

--- node/src/node/config/config_base.js
+++ node/src/node/config/config_base.js
@@ -9,7 +9,7 @@
 			{ "regex" : "/node(/.*)?", "pattern" : "$0" },
 	//		{ "regex" : "/client(/.*)?", "pattern", : "/node$0" },
 			{ "regex" : "/media(/.*)?", "pattern" : "$0" },
-			{ "regex" : "/favicon\\.ico", "pattern" : "$0" },
+			{ "regex" : "/favicon\\.ico", "pattern" : "/node/static/images/favicon.ico" },
 			{ "regex" : ".*", "pattern" : "/node" }
 		]
 	},
@@ -26,14 +26,10 @@
 		"timeout" : 604800,  
 		"location" : "client",  
 		"client" :      {  
-//			"cbc" : "aes256",
-//			"cbc_key" : "4bf2cc8ba8",
-//			"encryptor" : "aes256",
 		    "hmac" :        "sha1",  
 		    "hmac_key" :    "3891bbf7f845fd4277008a63d72640fc13bb9a31"  ,
-//			"key" : "3894bf2cc8ba81bbf7f845fd42774bf2cc8ba8008a63d72644bf2cc8ba80fc4bf2cc8ba813bb9a31"
 		}      
-	    },  
+	},  
 	"config_noded" : {
 		"client" : {
 			"path" : "run/client",
