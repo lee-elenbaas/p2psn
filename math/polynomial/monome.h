6364d4eb1a98a5e7fd58387390e6e61fc4b17a0f
Lee Elenbaas <lee@elenbaas.org.il>
2013-05-06 17:47:33 +0300

    start of sparce polynome handling

---
+++ math/polynomial/monome.h
@@ -0,0 +1,22 @@
+/******************************
+ * A single monome with all its suppport operations
+ *****************************/
+#ifndef FULL_POLYNOMIAL_H
+#define FULL_POLYNOMIAL_H
+
+namespace lee {
+	namespace math {
+		namespace polynomial {
+			
+			/***************
+			 * Class for handling a single monome
+			 **************/
+			template<typename Field, typename FieldTraits>
+			class monome;
+			
+			
+		}
+	}
+}
+
+#endif
