e9e4d6315f09cb85d4c1e18919072b4eca5d350a
Lee Elenbaas <lee@elenbaas.org.il>
2014-05-03 07:57:59 +0300

    continue with objects structure

--- node/src/structure/entities/object.hpp
+++ node/src/structure/entities/object.hpp
@@ -5,28 +5,66 @@
 
 namespace p2psn {
 	namespace entities {
+		// id for finding an object in the repository and for identifing it across the p2psn network
+		class object_id; 
 		
-		class object_id;
-		class object;
+		// base class for all objects
+		class object; 
+		
+		// reference from one object to another
+		template<Type>
 		class object_reference;
 		
-		typedef std::string object_id; // TODO perhaps a uuid will be better, or a combination of uuid with some more information
+		// special types
+		class object_type;
+		class object_view;
 		
-		class object {
+		/////////////////////////////////////////////
+		
+		class object_id {
+			public:
+				std::string source_uri;
+				sts::string sid; // TODO: perhaps a uuid will be better as type
+				
+				std::string normalized() {
+					return source_uri + sid; // TODO: clean up the normalized string so it become a valid JS/Angular identifier
+				}
+		};
+		
+		class object : {
 			public:
 				object_id id;
 				int version;
 				
-				contact source; // The contact that created this object
+				object_reference<contact> source; // The contact that created this object
 				
-				object_reference type; // reference the type object this object belongs too
-				object_reference preferred_view; // reference to the preferred view used to render this object
+				object_reference<object_type> type; // the type object belongs too
+				object_reference<object_view> preferred_view; // the preferred view used to render this object in this p2psn profile (TODO: perhaps this information should sit somewhere inside the profile)
 				
 				std::string content; // TODO: perhaps a buffer will be better for performance, but keeping this a string will make this more language portable
 		};
 		
-		class object_reference {
-			object_id id;
+		namespace {
+			class object_reference_imp { // TODO: perhaps this should become the base class for object as well
+				public:
+					object_id id;
+			}
+		}
+		
+		template<Type>
+		class object_reference : public object_reference_imp {
+		};
+		
+		class object_type : public object {
+			public:
+				object_reference<object_view> recomended_view; // the recommended view for objects of this type
+		};
+		
+		class object_view : public object {
+			public: // TODO: this will contain API for getting the angular directive used for the display, and how to deliver the view object to that directive (or perhaps this will be commmon between all directives)
+				directiveName() {
+					return id.normalized();
+				}
 		};
 	} // namespace entities
 } // namespace p2psn
--- node/src/structure/transport/offline_transport.hpp
+++ node/src/structure/transport/offline_transport.hpp
@@ -1,16 +1,17 @@
 #ifndef __P2PSN_OFFLINE_TRANSPORT_H
 #define __P2PSN_OFFLINE_TRANSPORT_H
 
+#include <func>
 #include "../entities/contact.hpp"
 
 namespace p2psn {
 	namespace transport {
 		
-		typedef std::func<contact, content, nullptr> message_handler;
+		typedef std::func<object, nullptr> message_handler; // TODO: check syntax
 		
 		class offline_transport {
 			public:
-				void send_message(contact to, content msg) = 0;
+				void send_message(contact to, object msg) = 0;
 				
 				void register_message_handler(message_handler) = 0;
 				void unregister_message_handler(message_handler) = 0;
