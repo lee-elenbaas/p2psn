66831b59b3e715ae42a8a134149ac0f4c5f40c75
Lee Elenbaas <lee@elenbaas.org.il>
2014-07-22 21:04:15 +0300

    continue with the c++ structure

--- node/src/structure/repository/repository.cpp
+++ node/src/structure/repository/repository.cpp
@@ -25,6 +25,8 @@ namespace p2psn {
 
 				if (current.id() != obj.id())
 					throw id_conflict_error("Attempt to store a different object with a clashing id");
+					
+				if (current.
 				// TODO: need to check that new object is indeed a new version of the old object
 				// TODO: throw exception if object is not a different version of the same object, or if it is not a newer version
 			}
